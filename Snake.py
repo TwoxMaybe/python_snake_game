2a03e3cf911b04d2c3082d49187c53e18f30b970
Cristhian Navas <navascristhian43@gmail.com>
2026-01-17 11:44:56 -0600

    Refactor: Snake class fully improved

--- Snake.py
+++ Snake.py
@@ -1,88 +1,99 @@
 from turtle import Turtle
 
-# Constantes sugeridas para evitar "Magic Numbers"
-STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
-MOVE_DISTANCE = 20
+#Directions based in the global angle of reference used for Turtle
+Up = 90
+Down = 270
+Left = 180
+Right = 0
 
 class Snake:
-    def __init__(self):
-
-        # Atributo para almacenar los objetos Turtle (segmentos)
-        self.segments = []
+    def __init__(self) -> None:
+        self.segments = [] #Segments that compose the body of the snake (The first one is the head)
         self.create_snake()
 
-    def create_snake(self):
-        """Itera sobre las posiciones iniciales para crear el cuerpo."""
+    def create_snake(self) -> None:
+        """"
+        Create the initial body of the snake (head and 2 segments of body)
+        """
+        #Create and save the head
+        self.add_segment((0,0))
 
-        #Creamos los 3 primeros elementos de la serpiente
-        for starting_pos in STARTING_POSITIONS:
-            self.add_segment(starting_pos)
-
+        #For the body make the same but his position have to be behind the head
+        #We create 2 segments more to the initial Snake
+        for i in range(0, 2):
+            self.add_segment((i * -20, 0))
         return
 
-    def add_segment(self, position):
-        """Crea una nueva instancia de Turtle y la configura."""
+    def add_segment(self, position: tuple[int, int]) -> None:
+        """Create the segment and configurate his 
+        appearance to append it to the body of the snake.""""
 
-        #Creamos el segmento
+        #We create the object turtle
         segment = Turtle()
 
-        #Lo configuramos
+        #We configure his appearances
         segment.shape("square")
         segment.color("white")
-        segment.penup()
+        segment.penup() # We don't want to draw the followed path
         segment.setposition(position)
 
-        #Lo agregamos al cuerpo
+        #We append it to the body
         self.segments.append(segment)
 
         return
 
-    def move(self):
-        """Mueve la serpiente hacia adelante un paso."""
+    def move(self) -> None:
+        """Makes the automatic movement of the snake.
+        The logic is: The body follows the head, if one part of the body moves
+        the segment behind it takes his place"""
+
         last_segment_index = len(self.segments) - 1
 
-        #Para mover el cuerpo
+        #We go from the tail to the head
         for i in range(last_segment_index,0,-1):
-
             new_x = self.segments[i - 1].xcor()
             new_y = self.segments[i - 1].ycor()
             self.segments[i].setposition(new_x,new_y)
 
-        #Para mover la cabeza
+        #After all the movement of the body we move the head
         self.segments[0].forward(20)
 
         return
 
-    def up(self):
-        """Cambia la dirección de la cabeza hacia arriba."""
+    def up(self) -> None:
+        """Changes the direction of the snake to the up."""
 
-        #Evitar que choque la cabeza con su propio cuerpo
-        if not (int(self.segments[0].heading()) == 270):
+        #Avoid the crash of the head with the body (he can't turn 180 degrees )
+        if not (int(self.segments[0].heading()) == Down):
             self.segments[0].setheading(90)
-
         return
 
-    def down(self):
-        """Cambia la dirección de la cabeza hacia abajo."""
+    def down(self) -> None:
+        """Changes the direction of the snake to down."""
 
-        # Evitar que choque la cabeza con su propio cuerpo
-        if not (int(self.segments[0].heading()) == 90):
+        #Avoid the crash of the head with the body (he can't turn 180 degrees )
+        if not (int(self.segments[0].heading()) == Up):
             self.segments[0].setheading(270)
         return
 
-    def left(self):
+    def left(self) -> None:
+        """Changes the direction of the snake to the left."""
 
-        #Evitar que choque la cabeza con su propio cuerpo
+        ##Avoid the crash of the head with the body (he can't turn 180 degrees)
         if not (int(self.segments[0].heading()) == 0):
             self.segments[0].setheading(180)
+        return
 
-    def right(self):
+    def right(self) -> None:
+        """Changes the direction of the snake to the right."""
 
-        # Evitar que choque la cabeza con su propio cuerpo
-        if not (int(self.segments[0].heading()) == 180):
+        #Avoid the crash of the head with the body (he can't turn 180 degrees )
+        if not (int(self.segments[0].heading()) == Left):
             self.segments[0].setheading(0)
 
-    def extend(self):
-        """Añade un nuevo segmento a la serpiente."""
+        return
+
+    def extend(self) -> None:
+        """Appends a new segment to the body"""
         self.add_segment(self.segments[-1].position())
         return
